32169e66176e339d4f82b8e0df430d14e28788a2
fateru tobi <fateruvictor96@gmail.com>
2024-05-07 00:47:13 +0100

    update

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { MapComponent } from './component/map/map.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '', redirectTo: '/dd', pathMatch: 'full'
+  },
+  {
+    path:'home',
+    component: MapComponent
+  },
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
